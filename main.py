ae71c4621ae634a7c7da685b536491872b328dcd
ayoussef <youssef.ammari@ynov.com>
2024-10-24 14:07:21 +0200

    setup

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import msvcrt
 print("Appuie sur une touche, appuie sur 'q' pour quitter.")
 while True:
     if msvcrt.kbhit():
-        key = msvcrt.getch().decode('utf-8')
+        key = msvcrt.getch().decode('cp437')  # Remplacer 'utf-8' par 'cp437'
         print(f"Touche appuyée: {key}")
         if key == 'q':
             break
